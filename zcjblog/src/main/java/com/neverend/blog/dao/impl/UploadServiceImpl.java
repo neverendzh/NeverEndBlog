3c9fd51359d5fa7c9fb4efa782d8f745da1da909
neverendzh <neverend_zh@163.com>
2019-10-07 03:05:19 +0800

    修复七牛上传文件不指定key默认使用hash作为文件名没有后缀。 设置key，使用七牛hash算法得到hash拼接上文件后缀

--- zcjblog/src/main/java/com/neverend/blog/dao/impl/UploadServiceImpl.java
+++ zcjblog/src/main/java/com/neverend/blog/dao/impl/UploadServiceImpl.java
@@ -12,6 +12,7 @@ import com.neverend.blog.moudel.UploadFileMsg;
 import com.neverend.blog.security.CustomerFilterChainDefinition;
 import com.neverend.blog.service.AccountServiceMyzcj;
 import com.neverend.blog.service.UploadService;
+import com.neverend.blog.util.email.qiniu.Etag;
 import com.neverend.blog.util.email.redis.RedisUtil;
 import com.qiniu.common.QiniuException;
 import com.qiniu.http.Response;
@@ -23,6 +24,9 @@ import com.qiniu.storage.persistent.FileRecorder;
 import com.qiniu.util.Auth;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.apache.tika.mime.MimeType;
+import org.apache.tika.mime.MimeTypeException;
+import org.apache.tika.mime.MimeTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +34,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -60,57 +65,35 @@ public class UploadServiceImpl implements UploadService {
     @Override
     public UploadFileMsg uploadimg(MultipartFile multipartFile) {
         UploadFileMsg uploadFileMsg = new UploadFileMsg();
-        List<Object> objects = redisUtil.lGet(qiNiuKeyred + "0", 0L, -1L);
-        List<QiNiuKey> qiNiuKey = zhQiNiuKey(objects);
-        if (qiNiuKey == null) {
-            qiNiuKey = qiNiuKeyDao.getQiNiuKey("0");
-            redisUtil.lSet(qiNiuKeyred + "0", qiNiuKey, 7200);
-        }
+        QiNiuKey qiNiuKey = getqiniukey("0");
         Subject subject = SecurityUtils.getSubject();
         Account account = (Account) subject.getPrincipal();
-        if (!qiNiuKey.isEmpty()) {
+        if (qiNiuKey != null) {
             if (account != null && account.getBeiYongEr().equals("管理员")) {
-                //构造一个带指定 Region 对象的配置类
                 Configuration cfg = new Configuration(Region.huanan());
-                String accessKey = qiNiuKey.get(0).getAk();
-                String secretKey = qiNiuKey.get(0).getSk();
-                String bucket = qiNiuKey.get(0).getQiNiuName();
-//默认不指定key的情况下，以文件内容的hash值作为文件名
-                String key = null;
-                Auth auth = Auth.create(accessKey, secretKey);
-                String upToken = auth.uploadToken(bucket);
-                String localTempDir = Paths.get(System.getenv("java.io.tmpdir"), bucket).toString();
+                String key = hashName(multipartFile);
+                Auth auth = Auth.create(qiNiuKey.getAk(), qiNiuKey.getSk());
+                String upToken = auth.uploadToken(qiNiuKey.getQiNiuName());
+                String localTempDir = Paths.get(System.getenv("java.io.tmpdir"), qiNiuKey.getQiNiuName()).toString();
                 try {
-                    //设置断点续传文件进度保存目录
                     FileRecorder fileRecorder = new FileRecorder(localTempDir);
                     UploadManager uploadManager = new UploadManager(cfg, fileRecorder);
-                    try {
-                        Response response = uploadManager.put(multipartFile.getBytes(), key, upToken);
-                        //解析上传成功的结果
-                        DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
-//                    生成访问url
-                        String encodedFileName = URLEncoder.encode(putRet.key, "utf-8").replace("+", "%20");
-                        String publicUrl = String.format("%s/%s", qiNiuKey.get(0).getYuMing(), encodedFileName);
-//                      根据当前用户德尔id作为redis的键，将url存储进redis缓存，设置过期时间30min。
-//                      如果30分钟内没有点击发布或者预览，则不存储链接到数据库。
-                        redisUtil.set(account.getId(), publicUrl);
-//                    设置响应信息
-                        uploadFileMsg.setUrl(publicUrl);
-                        uploadFileMsg.setUploaded("1");
-                        uploadFileMsg.setFileName(putRet.key);
-                        return uploadFileMsg;
-                    } catch (QiniuException ex) {
-                        Response r = ex.response;
-                        logger.error(r.toString());
-                        return seErroMsg(uploadFileMsg, ex);
-                    }
+                    Response response = uploadManager.put(multipartFile.getBytes(), key, upToken);
+                    DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+                    String encodedFileName = URLEncoder.encode(putRet.key, "utf-8").replace("+", "%20");
+                    String publicUrl = String.format("%s/%s", qiNiuKey.getYuMing(), encodedFileName);
+                    redisUtil.set(account.getId(), publicUrl);
+                    uploadFileMsg.setUrl(publicUrl);
+                    uploadFileMsg.setUploaded("1");
+                    uploadFileMsg.setFileName(putRet.key);
+                    return uploadFileMsg;
                 } catch (IOException ex) {
+                    logger.error(ex.getMessage());
                     return seErroMsg(uploadFileMsg, ex);
                 }
             } else {
                 return seErroMsg(uploadFileMsg, new ServiceException("您没有无权限"));
             }
-
         } else {
             logger.error("获取七牛 ak sk 失败");
             return seErroMsg(uploadFileMsg, new ServiceException("获取七牛 ak sk 失败"));
@@ -126,70 +109,97 @@ public class UploadServiceImpl implements UploadService {
     @Override
     public Msg upLoadAccountImg(MultipartFile img) {
         Msg msg = new Msg();
-//        获取上传所需七牛key 空间 域名
-        List<Object> objects = redisUtil.lGet(qiNiuKeyred + "1", 0L, -1L);
-        List<QiNiuKey> qiNiuKey = zhQiNiuKey(objects);
-        if (qiNiuKey == null) {
-            qiNiuKey = qiNiuKeyDao.getQiNiuKey("1");
-            redisUtil.lSet(qiNiuKeyred + "1", qiNiuKey, 7200);
-        }
-        Subject subject = SecurityUtils.getSubject();
-        Account account = (Account) subject.getPrincipal();
-        if (!qiNiuKey.isEmpty()) {
+        QiNiuKey qiNiuKey = getqiniukey("1");
+        Account account = (Account) SecurityUtils.getSubject().getPrincipal();
+        if (qiNiuKey != null) {
             if (account != null && account.getBeiYongEr().equals("管理员") ||
                     account.getBeiYongEr().equals("用户") || account.getBeiYongEr().equals("超级管理员")) {
-                //构造一个带指定 Region 对象的配置类
                 Configuration cfg = new Configuration(Region.huanan());
-                String accessKey = qiNiuKey.get(0).getAk();
-                String secretKey = qiNiuKey.get(0).getSk();
-                String bucket = qiNiuKey.get(0).getQiNiuName();
-                //默认不指定key的情况下，以文件内容的hash值作为文件名
-                String key = null;
-                Auth auth = Auth.create(accessKey, secretKey);
-                String upToken = auth.uploadToken(bucket);
-                String localTempDir = Paths.get(System.getenv("java.io.tmpdir"), bucket).toString();
                 try {
-                    //设置断点续传文件进度保存目录
+                    String key = hashName(img);
+                    Auth auth = Auth.create(qiNiuKey.getAk(), qiNiuKey.getSk());
+                    String upToken = auth.uploadToken(qiNiuKey.getQiNiuName(), key);
+                    String localTempDir = Paths.get(System.getenv("java.io.tmpdir"), qiNiuKey.getQiNiuName()).toString();
                     FileRecorder fileRecorder = new FileRecorder(localTempDir);
                     UploadManager uploadManager = new UploadManager(cfg, fileRecorder);
-                    try {
-                        Response response = uploadManager.put(img.getBytes(), key, upToken);
-                        //解析上传成功的结果
-                        DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
-//                    生成访问url
-                        String encodedFileName = URLEncoder.encode(putRet.key, "utf-8").replace("+", "%20");
-                        String publicUrl = String.format("%s/%s", qiNiuKey.get(0).getYuMing(), encodedFileName);
-//                      根据当前用户德尔id作为redis的键，将url存储进redis缓存，设置过期时间30min。
-//                      如果30分钟内没有点击发布或者预览，则不存储链接到数据库。
-                        redisUtil.set(account.getId(), publicUrl);
-//                    设置响应信息
-                        Account account1 = new Account();
-                        account1.setId(account.getId());
-                        account1.setBeiYongSan(publicUrl);
-                        accountServiceMyzcj.updateaccount(account1);
-
-                        Map<String, String> msgsrc = new HashMap<>();
-                        msgsrc.put("src", publicUrl);
-                        msg.setCode("0");
-                        msg.setMsg("成功");
-                        msg.setData(msgsrc);
-                        return msg;
-                    } catch (QiniuException ex) {
-                        Response r = ex.response;
-                        logger.error(r.toString());
-                        return seErroMsg(msg, ex);
-                    }
+                    Response response = uploadManager.put(img.getBytes(), key, upToken);
+                    DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
+                    String encodedFileName = URLEncoder.encode(putRet.key, "utf-8").replace("+", "%20");
+                    String publicUrl = String.format("%s/%s", qiNiuKey.getYuMing(), encodedFileName);
+                    redisUtil.set(account.getId(), publicUrl);
+                    updateimg(account, publicUrl);
+                    retmsg(msg, publicUrl);
+                    return msg;
                 } catch (IOException ex) {
                     return seErroMsg(msg, ex);
                 }
             } else {
-//                没有权限
                 return seErroMsg(msg, new ServiceException("您没有权限"));
             }
         } else {
             logger.error("获取七牛 ak sk 失败");
             return seErroMsg(msg, new ServiceException("服务暂停"));
         }
+    }
+
+    private String hashName(MultipartFile img) {
+        try {
+            String contentType = img.getContentType();
+            MimeTypes allTypes = MimeTypes.getDefaultMimeTypes();
+            MimeType jpeg = allTypes.forName(contentType);
+            String houzhui = jpeg.getExtension(); //文件后缀
+            InputStream inputStream = img.getInputStream();
+            String stream = Etag.stream(inputStream, img.getSize()) + houzhui;
+            inputStream.close();
+            return stream;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MimeTypeException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static byte[][] TwoArry(byte[] bytes) {
+        byte[][] arr = new byte[1][bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            arr[0][i] = bytes[i];
+        }
+        return arr;
+    }
+
+    private void retmsg(Msg msg, String publicUrl) {
+        Map<String, String> msgsrc = new HashMap<>();
+        msgsrc.put("src", publicUrl);
+        msg.setCode("0");
+        msg.setMsg("成功");
+        msg.setData(msgsrc);
+    }
+
+    private int updateimg(Account account, String publicUrl) {
+        Account account1 = new Account();
+        account1.setId(account.getId());
+        account1.setBeiYongSan(publicUrl);
+        int updateaccount = accountServiceMyzcj.updateaccount(account1);
+        return updateaccount;
+    }
+
+    /**
+     * 获取七牛key
+     *
+     * @return
+     */
+    private QiNiuKey getqiniukey(String key) {
+        List<Object> objects = redisUtil.lGet(qiNiuKeyred + key, 0L, -1L);
+        List<QiNiuKey> qiNiuKey = zhQiNiuKey(objects);
+        if (qiNiuKey == null) {
+            qiNiuKey = qiNiuKeyDao.getQiNiuKey(key);
+            redisUtil.lSet(qiNiuKeyred + key, qiNiuKey, 7200);
+        }
+        if (!qiNiuKey.isEmpty() && qiNiuKey.size() > 0) {
+            return qiNiuKey.get(0);
+        }
+        return null;
     }
 
     private List<QiNiuKey> zhQiNiuKey(List<Object> objects) {
@@ -212,5 +222,9 @@ public class UploadServiceImpl implements UploadService {
         msg.setCode(Code.error);
         msg.setMsg(Code.errorMsg);
         return msg;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(3 % 3);
     }
 }
